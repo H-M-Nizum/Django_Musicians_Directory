481f97689d1a188e29c650c48ff89072931da732
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-07 00:22:49 +0600

    Django_Musicians_Directory project

---
+++ Musicians_Directory/musician/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Musician(models.Model):
+    First_Name = models.CharField(max_length=50) 
+    Last_Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Phone_number = models.CharField(max_length=12)
+    Instrument_Type = models.CharField(max_length=100) 
+    
+    
+    
+    def __str__(self):
+        return f"{self.First_Name} {self.Last_Name}"
